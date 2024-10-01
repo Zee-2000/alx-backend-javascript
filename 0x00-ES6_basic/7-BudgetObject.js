505164e91fb4d8f38a60c6d9c6d994697093f707
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-10-01 22:22:03 +0300

    BudgetObject

---
+++ 0x00-ES6_basic/7-BudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, captia){
+    const budget = 
+    {
+        income, 
+        gdp, 
+        captia
+    };
+    return budget;
+}
---
+++ 0x00-ES6_basic/7-main.js
@@ -0,0 +1,2 @@
+import getBudgetObject from "./7-BudgetObject.js";
+console.log(getBudgetObject(400, 700, 900));
