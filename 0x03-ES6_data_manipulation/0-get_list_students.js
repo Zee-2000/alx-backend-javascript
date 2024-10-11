6f4a35a7c9bea3708014c4abe0ce209ffbb51993
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-10-11 22:57:20 +0300

    getStudentsList

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getStudentsList(){
+    return [
+        {id: 1, firstName : "Guillame", location: "San Fransisco"},
+        {id: 2, firstName: "James", location:"Columbia"},
+        {id: 5, firstName:"Serna", location:"San Fransisco"}
+    ]
+}
