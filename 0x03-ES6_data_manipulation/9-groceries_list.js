66a33d9bff3cb6af3194d88e5793ecdb6e9c7979
Zee-2000 <zahwaabdelnasser@gmail.com>
2024-10-12 00:26:16 +0300

    newMap

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList(){
+    return new Map(
+    [
+        ["Apples", 10],
+        ["Tomaotoes", 10],
+        ["Pasta", 10],
+        ["Rice", 1],
+        ["Banana", 1]
+
+    ],)
+}
